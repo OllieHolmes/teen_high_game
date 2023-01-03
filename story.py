8602b8cb0842b3240d788f0df4c36c5791e995f4
OllieHolmes <22922920+OllieHolmes@users.noreply.github.com>
2023-01-03 23:36:49 +0100

    Add 'start_of_day' function

--- story.py
+++ story.py
@@ -4,14 +4,22 @@ import time
 
 def welcome_to_teen_high(player_name):
     print(f"""
-    Welcome to Teen High. You are a transfer student, and today is your first day.
-    How exciting!!
+    Welcome to Teen High. You are a transfer student, 
+    and today is your first day. How exciting!!
     
-    There are so many possibilities for you. 
-    Go to class and study, make friends, join clubs, and become the most popular teen in school.
+    Unfortunately there's only 5 more days of school before summer break.
+    Don't let that intimidate you though.
+    
+    There are so many possibilities for you: 
+    Go to class and study, make friends, join clubs, 
+    and become the most popular teen in school.
     Or fail and remain unknown. The choice is up to you. 
     
     Good luck on your new adventure, {player_name}!""")
-    read_delay(30)
-    new_day_divider()
+
+    read_delay()
+
+def start_of_day(day):
+    print(f"It's the start of Day {str(day)}. What do you want to do?")
     new_line()
+
