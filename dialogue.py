4550cfb0ddbac37e3d90bcf29496fe030b916437
OllieHolmes <22922920+OllieHolmes@users.noreply.github.com>
2023-01-05 00:21:25 +0100

    Add Dialogue lines

---
+++ dialogue.py
@@ -0,0 +1,46 @@
+# ----- SASSY LINES ----- #
+
+sassy_goodbye = ["I'm not asking you to leave, I'm telling you to leave.",
+                 "I didn't ask for your opinion, and I don't want it. Bye!",
+                 "I don't have time for your nonsense, bye.", "I'd rather be alone than be around you.",
+                 "I'm busy being fabulous, go bother someone else.",
+                 "I'm not interested in whatever it is you have to say, so please leave me alone."]
+
+sassy_whatsup = ["What's your deal today?",
+                 "What's the latest drama you're causing?",
+                 "What kind of trouble are you getting into today?",
+                 "What kind of mischief are you planning now?",
+                 "What kind of chaos are you stirring up today?",
+                 "What kind of trouble are you causing now?"]
+
+sassy_accept_date = ["I'm not really interested, but sure, I'll go on a date with you.",
+                     "I have better things to do, but I guess I can go on a date with you.",
+                     "I'm not really interested in you, but I'm bored, so sure, I'll go on a date with you.",
+                     "I'm not really attracted to you, but sure, I'll go on a date with you.",
+                     "I'm not really into you, but I'm feeling generous, so sure, I'll go on a date with you.",
+                     "I'm not really interested, but I'm feeling adventurous, so sure, I'll go on a date with you.",
+                     "Why not? I'm feeling reckless and I've got nothing better to do, so sure, I'll go on a date with you."]
+
+# ---- SHY LINES ---- #
+
+shy_accept_study_session = [[
+    "I-I don't know, I'm not really good at studying with other people... but I guess I could give it a try.",
+    "I-I'm not sure, I get a little nervous around new people... but I guess the library is a good place to study, so maybe it would be okay.",
+    "I-I don't know, I'm not really comfortable being around people I don't know well... but I guess the library is a safe place and it might be a good opportunity to make some new friends.",
+    "I-I'm not sure, I get flustered easily and I'm not very good at talking to people I don't know well... but I guess the library is a quiet place and it might be a good opportunity to focus on my studies.",
+    "I-I don't know, I'm not really comfortable being around so many people... but I guess the library is a good place to study and it might be a good opportunity to get some work done.",
+    "I-I'm not sure, I get a little nervous when I'm around new people... but I guess the library is a safe place and it might be a good opportunity to meet some new people and focus on my studies."],
+    [
+        "Oh, I'd love to! I'm really excited to study with you, but I'm a little nervous too... I hope I don't mess anything up.",
+        "Yes, I'd love to! I'm so excited to study with you, but I'm a little nervous about being around new people... I hope I don't do anything embarrassing.",
+        "Sure, I'd love to! I'm really excited to study with you, but I'm a little nervous about being in the library with so many people... I hope I don't get too flustered.",
+        "Sure, I'd love to! I'm really excited to study with you, but I'm a little nervous about being in the library with so many people... I hope I don't get too distracted."]]
+
+# ---- LOVING LINES ---- #
+
+loving_hangout_later = ["I can't get enough of you, let's hang out later.",
+                        "I'd love to spend some more time with you later.",
+                        "I can't get you out of my head, let's hang out later.",
+                        "I can't wait to see you again later.",
+                        "I've been thinking about you all day, let's hang out later.",
+                        "I just can't get enough of you, let's hang out later."]
